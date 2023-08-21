4c555799bee3fd4420dd714fc2cbc5c55077c3af
root <aruammaduabuchi@gmail.com>
2023-08-21 14:38:30 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string to stdout
+ * @str: string to print
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
