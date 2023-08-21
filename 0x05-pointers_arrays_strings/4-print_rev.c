d5896cf7d4a0b783a03589ce458d2a58788f27c6
root <aruammaduabuchi@gmail.com>
2023-08-21 14:55:41 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,19 @@
 
 void print_rev(char *s)
 {
-	int len = 0, int o;
+	int len = 0;
+       	int o;
 
 	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-	s--
+	s--;
 	for (o = len; o > 0 ; 0--)
 	{
 		_putchar(*s);
-		s--
+		s--;
 	}
-	_putchar(\n)
+	_putchar(\n);
 }
